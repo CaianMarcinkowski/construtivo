3eef712fe6de8622e17eb935400fb97caae40a73
CaianMarcinkowski <caianferre@gmail.com>
2023-09-01 16:40:30 -0300

    alterações finais

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const fs = require('fs');
 const bodyParser = require('body-parser')
 const http = require('http');
+const path = require('path');
 const app = express()
 
-const PORT = 3000
+const PORT = 8080
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -18,14 +19,19 @@ app.listen(PORT, () => {
 
 app.post('/', (req, res) => {
     console.log('Texto do arquivo: '+req.body.mensagem)
-    const data = 'Texto do arquivo par teste da CONSTRUTIVO: '+req.body.mensagem
+    const data = 'Texto do arquivo para teste da CONSTRUTIVO: '+req.body.mensagem
 
     fs.writeFile('arquivo.txt', data, (err) => {
         if (err) throw err;
       console.log('O arquivo foi criado!');
     });
- 
-    res.send('Local onde o arquivo esta armazenado: -> /home/caian/Documents/Projects/teste_construtivo/arquivo.txt')
+    
+    res.send('Link de acesso do download do arquivo construtivo: -> http://15.228.166.167:3000/download')
+})
+
+app.get('/download', (req, res) => {
+    var file = __dirname + '/../arquivo.txt';
+    res.download(file)
 })
 
 app.get('/teste', (req, res) => {
